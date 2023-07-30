7a2575e1c240a699877e58d691d487fccef29d99
Rob de Graaf <r.degraaf@cam.nl>
2023-07-30 10:40:20 +0200

    push

--- custom_components/awtrix-screen/sensor.py
+++ custom_components/awtrix-screen/sensor.py
@@ -5,7 +5,7 @@ import requests
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.helpers.config_validation import CONF_API_ENDPOINT
+from homeassistant.const import CONF_API_ENDPOINT
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import track_time_interval
