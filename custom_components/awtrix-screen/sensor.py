5fdbc3aad7fd70171e2b2dd3c2a330af48efedf4
Rob de Graaf <r.degraaf@cam.nl>
2023-07-30 10:24:28 +0200

    first

--- custom_components/awtrix-screen/sensor.py
+++ custom_components/awtrix-screen/sensor.py
@@ -1,38 +1,87 @@
-"""Platform for sensor integration."""
-from __future__ import annotations
+import logging
+from datetime import timedelta
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
+import requests
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_API_ENDPOINT, CONF_NAME
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.event import track_time_interval
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_SCAN_INTERVAL = 5  # Default scan interval in seconds
+
+DEFAULT_NAME = "Custom Screen Sensor"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_API_ENDPOINT): cv.string,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional("scan_interval", default=DEFAULT_SCAN_INTERVAL): cv.positive_int,
+    }
 )
-from homeassistant.const import TEMP_CELSIUS
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
-) -> None:
-    """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the custom sensor platform."""
+    api_endpoint = config.get(CONF_API_ENDPOINT)
+    name = config.get(CONF_NAME)
+    scan_interval = timedelta(seconds=config.get("scan_interval"))
+
+    sensors = [CustomScreenSensor(name, api_endpoint, scan_interval)]
+
+    add_entities(sensors)
+
+    def update_sensors(event_time):
+        """Update all the sensors."""
+        for sensor in sensors:
+            sensor.update()
+
+    # Schedule the update function based on the scan_interval
+    track_time_interval(hass, update_sensors, scan_interval)
 
 
-class ExampleSensor(SensorEntity):
-    """Representation of a Sensor."""
+class CustomScreenSensor(Entity):
+    """Representation of a Custom Screen Sensor."""
 
-    _attr_name = "Example Temperature"
-    _attr_native_unit_of_measurement = TEMP_CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
-    _attr_state_class = SensorStateClass.MEASUREMENT
+    def __init__(self, name, api_endpoint, scan_interval):
+        """Initialize the sensor."""
+        self._name = name
+        self._api_endpoint = api_endpoint
+        self._state = None
+        self._attributes = {}
+        self._scan_interval = scan_interval
 
-    def update(self) -> None:
-        """Fetch new state data for the sensor.
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
 
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._attr_native_value = 23
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+
+    def update(self):
+        """Fetch new state data for the sensor."""
+        try:
+            response = requests.get(self._api_endpoint, timeout=5)
+            if response.status_code == 200:
+                data = response.json()
+                if isinstance(data, list):
+                    self._state = data[0]
+                    self._attributes["screen"] = data  # Store the entire data list as the "screen" attribute
+                else:
+                    _LOGGER.warning("Invalid data format received from API.")
+            else:
+                _LOGGER.warning("Request to API failed with status code: %s", response.status_code)
+        except requests.exceptions.RequestException as e:
+            _LOGGER.warning("Error fetching data from API: %s", e)
